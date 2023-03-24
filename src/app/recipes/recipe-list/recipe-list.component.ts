8ee539f5976fdf63a9dc280326395a3145ad8ec9
sidharthatri12 <sidharth_atri@epam.com>
2023-03-24 19:24:37 +0530

    basic component setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://media.4rgos.it/i/Argos/jacket%20potatos?maxW=1200&qlt=75&fmt.jpeg.interlaced=true')
+  ];
 
   constructor() { }
 
